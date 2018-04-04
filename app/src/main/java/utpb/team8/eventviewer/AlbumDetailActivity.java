907c50499ea21b33964342ae54187088bada8fa5
louis11jr7 <louis11jr7@gmail.com>
2018-04-04 15:48:19 -0500

    AlbumDetail
    Set up the gridView for the album expansion. This will show the album in a grid view when clicked. The images are clickable but right now only diplay their position in the array. There will be additional expansion added to these.

--- app/src/main/java/utpb/team8/eventviewer/AlbumDetailActivity.java
+++ app/src/main/java/utpb/team8/eventviewer/AlbumDetailActivity.java
@@ -2,7 +2,11 @@ package utpb.team8.eventviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*When an album listing is expanded this class controls the corresponding individual album page
 Each individual album page will load different information but it will all be loaded in the same way as defined in this class
@@ -16,7 +20,18 @@ public class AlbumDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_album_detail);
 
         String savedExtra = getIntent().getStringExtra("Title");
-        TextView myText = (TextView) findViewById(R.id.textID);
+        TextView myText = (TextView) findViewById(R.id.albumTitle1);
         myText.setText(savedExtra);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(AlbumDetailActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
